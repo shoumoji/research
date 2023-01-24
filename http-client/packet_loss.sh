ce09eaf8af985ea88c3abcf787158c5a7e028090
ayumin <58648147+shoumoji@users.noreply.github.com>
2023-01-24 20:04:42 +0900

    fix: delayも含めてループさせる
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- http-client/packet_loss.sh
+++ http-client/packet_loss.sh
@@ -5,9 +5,21 @@ RESULT_DIR="${CURDIR}/results"
 
 tc qdisc del dev enp6s0 root
 
-for ((i = 5; i <= 100; i += 5)); do
-	file_num=$(printf "%02d\n" "${i}")
-	tc qdisc add dev enp6s0 root netem loss "${i}%"
-	go run "${CURDIR}/main.go" --count 100 --format csv --http3 "https://server:18000" >"${RESULT_DIR}/packet_loss_${file_num}%.csv"
-	tc qdisc del dev enp6s0 root
+for ((i = 0; i <= 100; i += 5)); do
+	for ((j = 0; j <= 500; i += 100)); do
+		packet_loss=$(printf "%03d\n" "${i}")
+		if ((i != 0)); then
+			tc qdisc add dev enp6s0 root netem loss "${i}%"
+		fi
+
+		ping_ms=$(printf "%03d\n" "${j}")
+		if ((j != 0)); then
+			tc qdisc add dev enp6s0 root netem delay "${j}ms"
+		fi
+
+		go run "${CURDIR}/main.go" --count 100 --format csv --http3 "https://server:18000" \
+			>"${RESULT_DIR}/packet_loss_${packet_loss}}%-ping_${ping_ms}ms.csv"
+
+		tc qdisc del dev enp6s0 root
+	done
 done
