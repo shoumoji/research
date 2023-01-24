952a0a7dd9e42f5e69f0ca82f301aafb8b4abafd
ayumin <58648147+shoumoji@users.noreply.github.com>
2023-01-24 15:50:05 +0900

    fix
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- http-client/packet_loss.sh
+++ http-client/packet_loss.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 for i in {0..99}; do
+	file=$(printf "%02d\n" "${i}")
 	tc qdisc add dev enp6s0 root netem loss "${i}%"
-	go run main.go --count 100 --format csv >"packet_loss_${i}%.csv"
+	go run main.go --count 100 --format csv >"packet_loss_${file}%.csv"
 	tc qdisc del dev enp6s0 root
 done
