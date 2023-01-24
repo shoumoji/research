1b83aa14536a19a63532a6503e9dc6ebdc51cbe9
ayumin <58648147+shoumoji@users.noreply.github.com>
2023-01-24 16:06:21 +0900

    fix
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- http-client/packet_loss.sh
+++ http-client/packet_loss.sh
@@ -3,8 +3,8 @@
 CURDIR=$(pwd)
 
 for ((i = 5; i <= 100; i += 5)); do
-	file=$(printf "%02d\n" "${i}")
+	file_num=$(printf "%02d\n" "${i}")
 	tc qdisc add dev enp6s0 root netem loss "${i}%"
-	go run "${CURDIR}/main.go" --count 100 --format csv --http3 https://server:18000 >"packet_loss_${file}%.csv"
+	go run "${CURDIR}/main.go" --count 100 --format csv --http3 "https://server:18000" >"packet_loss_${file_num}%.csv"
 	tc qdisc del dev enp6s0 root
 done
