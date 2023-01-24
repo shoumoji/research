af717516c793071eba2b1417d6c92a2b769d908f
ayumin <58648147+shoumoji@users.noreply.github.com>
2023-01-24 20:18:23 +0900

    fix
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- http-client/packet_loss.sh
+++ http-client/packet_loss.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 CURDIR=$(pwd)
 RESULT_DIR="${CURDIR}/results"
 
 tc qdisc del dev enp6s0 root
 
 for ((i = 0; i <= 100; i += 5)); do
-	for ((j = 0; j <= 500; i += 100)); do
+	for ((j = 0; j <= 500; j += 100)); do
 		packet_loss=$(printf "%03d\n" "${i}")
 		if ((i != 0)); then
 			tc qdisc add dev enp6s0 root netem loss "${i}%"
@@ -14,6 +16,7 @@ for ((i = 0; i <= 100; i += 5)); do
 
 		ping_ms=$(printf "%03d\n" "${j}")
 		if ((j != 0)); then
+			echo "j: $j"
 			tc qdisc add dev enp6s0 root netem delay "${j}ms"
 		fi
 
