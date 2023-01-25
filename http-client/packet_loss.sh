8ab32f13bba76a8bddd4aa78865360404df87887
ayumin <58648147+shoumoji@users.noreply.github.com>
2023-01-25 09:45:26 +0900

    fix
    Signed-off-by: ayumin <58648147+shoumoji@users.noreply.github.com>

--- http-client/packet_loss.sh
+++ http-client/packet_loss.sh
@@ -10,7 +10,7 @@ tc qdisc del dev enp6s0 root || true
 echo "initialize done"
 
 for ((i = 0; i <= 60; i += 5)); do
-	for ((j = 0; j <= 500; j += 100)); do
+	for ((j = 0; j <= 300; j += 100)); do
 		packet_loss=$(printf "%03d\n" "${i}")
 		if ((i != 0)); then
 			echo "packet_loss: ${i}%"
