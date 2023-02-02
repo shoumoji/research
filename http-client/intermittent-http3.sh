f46e3eab839c46e600ea307e37aec3bdeb75b9c0
ayumin <kosyu79@gmail.com>
2023-02-02 21:05:08 +0900

    断続的に接続するスクリプトを作成
    Signed-off-by: ayumin <kosyu79@gmail.com>

---
+++ http-client/intermittent-http3.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CURDIR=$(pwd)
+RESULT_DIR="${CURDIR}/intermittent-http3-results"
+
+echo "start initialize..."
+
+tc qdisc del dev enp6s0 root || true
+
+if [ ! -d "${RESULT_DIR}" ]; then
+	mkdir "${RESULT_DIR}"
+fi
+
+echo "initialize done"
+
+for ((i = 0; i <= 50; i += 5)); do
+	for ((j = 0; j <= 300; j += 100)); do
+		packet_loss=$(printf "%03d\n" "${i}")
+		ping_ms=$(printf "%03d\n" "${j}")
+
+		echo "packet_loss: ${packet_loss}%, i: ${i}"
+		echo "ping_ms: ${ping_ms}ms, j: ${j}"
+
+		if ((i != 0 && j != 0)); then
+			tc qdisc add dev enp6s0 root netem loss "${i}%" delay "${j}ms"
+		elif ((i == 0 && j != 0)); then
+			tc qdisc add dev enp6s0 root netem delay "${j}ms"
+		elif ((i != 0 && j == 0)); then
+			tc qdisc add dev enp6s0 root netem loss "${i}%"
+		fi
+
+		if ((k = 0; k < 100; k++)); do
+			go run "${CURDIR}/main.go" --count 1 --format csv --http3 "https://server:18000" \
+				>"${RESULT_DIR}/ping_${ping_ms}ms-packet_loss_${packet_loss}%.csv"
+		done
+
+		# パケロスも遅延もない時はエラーが出るため
+		if ((i != 0 || j != 0)); then
+			tc qdisc del dev enp6s0 root || true
+		fi
+	done
+done
